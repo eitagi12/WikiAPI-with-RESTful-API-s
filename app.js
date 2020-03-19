444381dcea4b165ee8a61ea5ee014b183f679680
Supasit Loha <supasit.nus@gmail.com>
2020-03-19 14:07:53 +0700

    [Added] - Regular Use

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const articleShema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleShema);
+
+///// Get all articles
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticle) {
+    if (!err) {
+      res.send(foundArticle);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+///// Add a new article
+app.post("/articles", function(req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send("Successfully added new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+///// Delete All articles (mongoose use deleteMany)
+app.delete("/articles", function(req, res) {
+  Article.deleteMany(function(err) {
+    if (!err) {
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
